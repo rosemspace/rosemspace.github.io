ec88c40528404e325fc5f3915996b5c9dc926abe
Roman Shevchenko <iroman.via@gmail.com>
2020-04-05 14:29:34 +0300

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,14 @@ set -e
 # build
 npm run build
 
+# create a build output directory if it doesn't not exist
+mkdir -p dist
+
 # navigate into the build output directory
 cd dist
+
+# remove the previous git folder if it still exists for some reasons
+rm -rf .git
 
 # initialize git
 git init
@@ -21,10 +27,13 @@ git add -A
 git commit -m 'deploy'
 
 # deploy to https://rosemspace.github.io
-git push -f git@github.com:rosemspace/rosemspace.github.io.git master
+git push -f https://github.com/rosemspace/rosemspace.github.io.git master
 
 # remove git folder
 rm -rf .git
 
 # go back
 cd -
+
+# uncomment this line and comment `set -e` if you want to see the errors (useful on Windows)
+#read -p "Press [Enter] key to exit..."
